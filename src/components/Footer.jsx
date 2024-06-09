e3f462d3d0a361b6761cda2b1153013687bb605f
Basith-m <abm2k20@gmail.com>
2024-06-09 08:42:11 +0530

    Footer changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer bg-body-tertiary mt-5 py-3">
-      <div className="container text-center text-md-left">
+      <div className="container text-center text-md-left d-flex flex-column gap-3">
         <div className="d-flex flex-wrap justify-content-around align-items-center">
           <div className="p-2">
             <a href="mailto:info@desertsafaridubai.com" className="footer-link">
@@ -18,6 +18,11 @@ function Footer() {
           <div className="p-2">
             <a href="tel:+97142087466" className="footer-link">
               <i className="fas fa-phone"></i> +971-4-2087-466
+            </a>
+          </div>
+          <div className="p-2">
+            <a href="tel:+97142087466" className="footer-link">
+              <i className="fas fa-phone"></i> About Us
             </a>
           </div>
         </div>
