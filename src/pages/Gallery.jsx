aa9b28d1db7ea0491b77c9b10d685a5817929979
mohamedrishal <rishalmundekkat@gmail.com>
2024-06-10 08:31:28 +0530

    Galler updated

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,11 +1,11 @@
 // src/components/Gallery.js
-import React from 'react';
-import ImageGallery from 'react-image-gallery';
+import React from "react";
+import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
-import desert1 from '../assets/Carousel-1.jpg';
-import desert2 from '../assets/Carousel-2.jpg';
-import desert3 from '../assets/IMG_0803.jpg';
-import desert4 from '../assets/IMG_0913.jpg';
+import desert1 from "../assets/Carousel-1.jpg";
+import desert2 from "../assets/Carousel-2.jpg";
+import desert3 from "../assets/IMG_0803.jpg";
+import desert4 from "../assets/IMG_0913.jpg";
 // Import other images similarly
 
 const images = [
@@ -30,8 +30,11 @@ const images = [
 
 const Gallery = () => {
   return (
-    <div className="gallery-container">
-      <ImageGallery items={images} />
+    <div className="gallery d-flex flex-column align-items-center">
+      <h3 className="gallery-heading">Photo Gallery</h3>
+      <div className="gallery-container">
+        <ImageGallery autoPlay={true}  items={images} />
+      </div>
     </div>
   );
 };
